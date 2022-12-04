e916e38a35889f51574c11000e1711f2ef2ed530
Sushil1204 <sushilspundkar@gmail.com>
2022-12-04 17:55:32 +0530

    Redux store configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
-
+import { Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Navbar from './pages/Navbar';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 function App() {
   
   return (
-    <h1>Welcome</h1>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/profile/:userId" element={<ProfilePage/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import authReducer from "./state";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoreActions: [FLUSH,REGISTER,REHYDRATE,PAUSE,PURGE],
+    },
+  })
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate>
+        <Router>  
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
-)
+);
