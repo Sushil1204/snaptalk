d1b571a1539e81f235bbbd4fbc7b93854b898ea9
Sushil1204 <sushilspundkar@gmail.com>
2022-12-17 12:58:56 +0530

    HomePage created updated

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import CreatePost from '../components/CreatePost'
+import Navbar from '../components/Navbar'
+import ProfileCard from '../components/ProfileCard'
+import Post from '../components/Post'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Navbar />
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 pt-6 gap-2 justify-center">
+        <div className=''>
+          <ProfileCard/>
+        </div>
+        <div className='col-span-2'>
+          <CreatePost />
+          <Post/>
+        </div>
+      </div>
+      </div>
   )
 }
 
