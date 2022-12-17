e3b0bd65fbfbf2d8451872abb127e40a813c0db4
Sushil1204 <sushilspundkar@gmail.com>
2022-12-17 12:57:54 +0530

    login Page created

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,36 +1,296 @@
-import React from 'react'
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
-import Form from '../components/Form';
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import { MdOutlineEdit } from "react-icons/md";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "../state";
 
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+  location: yup.string().required("required"),
+  occupation: yup.string().required("required"),
+  picture: yup.string().required("required"),
+});
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+});
+
+const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
+
+const initialValuesLogin = {
+  email: "",
+  password: "",
+};
 const LoginPage = () => {
-  const theme = useTheme();
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-  return (
-    <Box>
-      <Box
-        width="100%"
-        backgroundColor={theme.palette.background.alt}
-        p="1rem 6%"
-        textAlign="center"
-      >
-        <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Snapbook
-        </Typography>
-      </Box>
-      <Box
-        width={isNonMobileScreens ? "50%" : "93%"}
-        p="2rem"
-        m="2rem auto"
-        borderRadius="1.5rem"
-        backgroundColor={theme.palette.background.alt}
-      >
-        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem",textAlign:"center" }}>
-          Welcome to Snapbook <br/> the Social Media for all generation!
-        </Typography>
-        <Form />
-      </Box>
-    </Box>
-  )
-}
+  const [pageType, setPageType] = useState("login");
+  const isLogin = pageType === "login";
+  const isRegister = pageType === "register";
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default LoginPage
+  const register = async (values, onSubmitProps) => {
+    // this allows us to send form info with image
+    const formData = new FormData();
+    for (let value in values) {
+      formData.append(value, values[value]);
+    }
+    formData.append("picturePath", values.picture.name);
+
+    const savedUserResponse = await fetch(
+      "http://localhost:3001/auth/register",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const savedUser = await savedUserResponse.json();
+    onSubmitProps.resetForm();
+
+    if (savedUser) {
+      setPageType("login");
+    }
+  };
+
+  const login = async (values, onSubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
+    const loggedIn = await loggedInResponse.json();
+    onSubmitProps.resetForm();
+    if (loggedIn) {
+      dispatch(
+        setLogin({
+          user: loggedIn.user,
+          token: loggedIn.token,
+        })
+      );
+      navigate("/home");
+    }
+  };
+
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    if (isLogin) await login(values, onSubmitProps);
+    if (isRegister) await register(values, onSubmitProps);
+  };
+
+  return (
+    <>
+      <div className="flex justify-center min-h-screen">
+        <div className="hidden bg-cover lg:block lg:w-2/5 bg-[url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')]"></div>
+
+        <div className="flex items-center w-full max-w-2xl p-8 mx-auto lg:px-12 lg:w-3/5">
+          <div className="w-full">
+            <h1 className="text-2xl font-semibold tracking-wider text-gray-900 capitalize">
+              Get your free account now.
+            </h1>
+
+            <p className="mt-4 text-gray-500 dark:text-gray-400">
+              Let’s get you all set up so you can verify your personal account
+              and begin setting up your profile.
+            </p>
+            <Formik
+              onSubmit={handleFormSubmit}
+              initialValues={
+                isLogin ? initialValuesLogin : initialValuesRegister
+              }
+              validationSchema={isLogin ? loginSchema : registerSchema}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  {isRegister && (
+                    <>
+                      <div className="grid grid-cols-1 gap-6 mt-4 md:grid-cols-2">
+                        <div>
+                          <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                            First Name
+                          </label>
+                          <input
+                            type="text"
+                            placeholder="John"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.firstName}
+                            name="firstName"
+                            className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-gray-300 dark:border-gray-700 "
+                          />
+                          <p>{touched.firstName && errors.firstName}</p>
+                        </div>
+
+                        <div>
+                          <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                            Last name
+                          </label>
+                          <input
+                            type="text"
+                            // placeholder="Snow"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.lastName}
+                            name="lastName"
+                            className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-gray-300 dark:border-gray-700 "
+                          />
+                          {touched.lastName && errors.lastName}
+                        </div>
+                      </div>
+                      <div className="grid grid-cols-1 gap-6 mt-4 md:grid-cols-2">
+                        <div>
+                          <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                            Location
+                          </label>
+                          <input
+                            type="text"
+                            placeholder="CAL"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.location}
+                            name="location"
+                            className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-gray-300 dark:border-gray-700 "
+                          />
+                          <p>{touched.location && errors.location}</p>
+                        </div>
+                        <div>
+                          <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                            Occupation
+                          </label>
+                          <input
+                            type="text"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.occupation}
+                            name="occupation"
+                            className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-gray-300 dark:border-gray-700 "
+                          />
+                          <p>
+                          {touched.occupation && errors.occupation}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="mt-8">
+                        <Dropzone
+                          acceptedFiles=".jpg,.jpeg,.png"
+                          multiple={false}
+                          onDrop={(acceptedFiles) =>
+                            setFieldValue("picture", acceptedFiles[0])
+                          }
+                        >
+                          {({ getRootProps, getInputProps }) => (
+                            <div
+                              {...getRootProps()}
+                              className="border-2 border-dashed border-gray-600 py-[20px] px-[10px] flex items-center justify-between w-full"
+                            >
+                              <input {...getInputProps()} />
+                              {!values.picture ? (
+                                <p>Add Picture Here</p>
+                              ) : (
+                                <div className="flex items-center justify-between">
+                                  <p>{values.picture.name}</p>
+                                  <span>
+                                    <MdOutlineEdit />
+                                  </span>
+                                </div>
+                              )}
+                            </div>
+                          )}
+                        </Dropzone>
+                      </div>
+                    </>
+                  )}
+
+                  <div className="mt-4">
+                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                      Email address
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="johnsnow@example.com"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.email}
+                      name="email"
+                      className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-gray-300 dark:border-gray-700 "
+                    />
+                    <p>{touched.email && errors.email}</p>
+                  </div>
+
+                  <div className="mt-4">
+                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="Enter your password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.password}
+                      name="password"
+                      className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:text-gray-300 dark:border-gray-700 "
+                    />
+                    <p>{touched.password && errors.password}</p>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="flex items-center justify-between w-full mt-4 px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  >
+                    <span>{isLogin ? "LOGIN" : "REGISTER"}</span>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 h-5 rtl:-scale-x-100"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  <p
+                    onClick={() => {
+                      setPageType(isLogin ? "register" : "login");
+                      resetForm();
+                    }}
+                  >
+                    {isLogin
+                      ? "Don't have an account? Sign Up here."
+                      : "Already have an account? Login here."}
+                  </p>
+                </form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
