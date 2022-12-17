704b9a5054b3964ab16314b4f2ce169779b271ad
Sushil1204 <sushilspundkar@gmail.com>
2022-12-17 12:47:37 +0530

    MUI replaced

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,15 @@
 import React, { useMemo } from 'react'
 import { Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Navbar from './pages/Navbar';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-import { themeSettings } from './theme.js';
 import { useSelector } from 'react-redux';
 
 function App() {
   const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
         <Routes>
           <Route path="/" element={<LoginPage/>}/>
           <Route
@@ -28,7 +21,6 @@ function App() {
               element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
           />
         </Routes>
-      </ThemeProvider>
     </div>
   )
 }
